3ba42e3c7178d751472b49324e93fb5346fd26cd
mutuyihao <96763582+mutuyihao@users.noreply.github.com>
2023-06-24 14:13:53 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,6 @@ import { defaultTheme } from 'vuepress'
 export default defineUserConfig({
     title: 'Mutu Admin 文档',
     description: 'Mutu Admin 文档',
-    base:'/doc/',
     theme: defaultTheme({
         // 默认主题配置
         logo: "logo.png",
